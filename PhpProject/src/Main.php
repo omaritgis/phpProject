81a5ab555e0a2e52437f482050c137fb1aa1ad35
Omar Totangy <omartotangy@gmail.com>
2020-06-23 01:47:06 +0200

    Added google API quickstart file and the credential files.

---
+++ PhpProject/src/Main.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Connection to Google Calendar API, create, edit, remove events in the calendar
+ * GUI = Calendar + 3 buttons
+ * Create 
+ * Edit
+ * Remove
+ * 
+ * Error-handling included
+ */
+
+//Author: Omar Totangy
+//Date: 2020-06-22
+
+class Main {
+    public string $cliID = "848866171385-ggsqnm73npounbv7sr7n3eqpvtpeh434.apps.googleusercontent.com";
+    public string $cliSec = "Cm_ig19tiWhTP23RiIsmQThc";
+    
+    function StartFunc() {
+        
+        
+    }
+}
+
