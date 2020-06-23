05bf4109e737fafe4a2833f8d99822b6362afd08
Omar Totangy <omartotangy@gmail.com>
2020-06-23 19:59:22 +0200

    Comment out section to include to website

--- PhpProject/src/quickstart.php
+++ PhpProject/src/quickstart.php
@@ -1,9 +1,9 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-if (php_sapi_name() != 'cli') {
-    throw new Exception('This application must be run on the command line.');
-}
+// if (php_sapi_name() != 'cli') {
+//     throw new Exception('This application must be run on the command line.');
+// }
 
 
 function getClient()
